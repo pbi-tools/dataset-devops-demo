c08676eaa6d4e3a156f14006074871d8806d0c75
Mathias Thierbach <mt-dev@outlook.com>
2022-07-04 01:10:48 +0100

    Resolves #5

--- Contoso Sales/Model/queries/Sales.m
+++ Contoso Sales/Model/queries/Sales.m
@@ -1,5 +1,5 @@
 let
-    Source = Web.Contents(SourceUrl & "Sales.csv"),
+    Source = Web.Contents(#"[SourceUrl]" & "Sales.csv"),
     Csv = Csv.Document(Source),
     #"Promoted Headers" = Table.PromoteHeaders(Csv, [PromoteAllScalars=true]),
     #"Changed Type" = Table.TransformColumnTypes(#"Promoted Headers",{{"StoreKey", Int64.Type}, {"ProductKey", Int64.Type}, {"PromotionKey", Int64.Type}, {"CurrencyKey", Int64.Type}, {"CustomerKey", Int64.Type}, {"OrderDateKey", Int64.Type},{"Order Line Number", Int64.Type}, {"Quantity", Int64.Type}, {"Unit Price", type number}, {"Unit Discount", type number}, {"Unit Cost", type number}, {"Net Price", type number}}),
