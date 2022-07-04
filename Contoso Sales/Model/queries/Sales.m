25f957402b828fae2d983facc71e50be18379164
Mathias Thierbach <mt-dev@outlook.com>
2022-07-04 01:16:11 +0100

    #8 [FilterDate] parameter for 'Sales' table

--- Contoso Sales/Model/queries/Sales.m
+++ Contoso Sales/Model/queries/Sales.m
@@ -3,6 +3,7 @@ let
     Csv = Csv.Document(Source),
     #"Promoted Headers" = Table.PromoteHeaders(Csv, [PromoteAllScalars=true]),
     #"Changed Type" = Table.TransformColumnTypes(#"Promoted Headers",{{"StoreKey", Int64.Type}, {"ProductKey", Int64.Type}, {"PromotionKey", Int64.Type}, {"CurrencyKey", Int64.Type}, {"CustomerKey", Int64.Type}, {"OrderDateKey", Int64.Type},{"Order Line Number", Int64.Type}, {"Quantity", Int64.Type}, {"Unit Price", type number}, {"Unit Discount", type number}, {"Unit Cost", type number}, {"Net Price", type number}}),
-    #"Parsed Date" = Table.TransformColumns(#"Changed Type",{{"Delivery Date", each Date.From(DateTimeZone.From(_)), type date}, {"Order Date", each Date.From(DateTimeZone.From(_)), type date}})
+    #"Parsed Date" = Table.TransformColumns(#"Changed Type",{{"Delivery Date", each Date.From(DateTimeZone.From(_)), type date}, {"Order Date", each Date.From(DateTimeZone.From(_)), type date}}),
+    Filtered_Date = if #"[FilterDate]" = null then #"Parsed Date" else Table.SelectRows(#"Parsed Date", each [Order Date] >= #"[FilterDate]")
 in
-    #"Parsed Date"
+    Filtered_Date
